f38eaa4c64b3250d410f6f0ecb2acaadf7e42369
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2024-10-02 21:25:09 +0200

    -  add mca test

--- test/test_node.js
+++ test/test_node.js
@@ -74,3 +74,49 @@ createApi().then((Module) => {
     console.log(instance.eigenValuesReduced2D);    
   
 });
+
+
+createApi().then((Module) => {
+
+    // instantiate COPASI simulator
+    var instance = new COPASI(Module);
+
+    // check version
+    console.log('Using COPASI: ', instance.version);
+
+    // load a local file using the filesystem 
+    var data = fs.readFileSync(process.argv[2], 'utf8');
+
+    // print model structure
+    console.log(instance.loadModel(data));
+ 
+    var selection = instance.selectionList;
+    selection.push("EE(J0,S1)")
+
+
+    console.log(selection);
+    instance.selectionList = selection;
+
+    var result = instance.simulateEx(0, 10, 11);
+    console.log(result);
+
+    // compute mca
+    instance.computeMca(true);
+
+    // print control coefficients
+    console.log("Flux control coefficients: ");
+    console.log(instance.getFluxControlCoefficients(true));
+    console.log(instance.getFluxControlCoefficients(false));
+
+    console.log("Concentration control coefficients: ");
+    console.log(instance.getConcentrationControlCoefficients(true));
+    console.log(instance.getConcentrationControlCoefficients(false));
+
+    console.log("Elasticities: ");
+    console.log(instance.getElasticities(true));
+    console.log(instance.getElasticities(false));
+
+    instance.reset();
+
+
+});

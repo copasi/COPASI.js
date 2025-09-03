4f17efa6a6c89ee7c1b7179925674377cebb8ac1
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2025-09-03 09:58:56 +0200

    - dont call node for web version

--- dev/em-web.sh
+++ dev/em-web.sh
@@ -9,13 +9,13 @@ rm -rf $INSTALL_PREFIX
 
 
 # copy the example files
-cp examples/node/*.js $INSTALL_PREFIX
+# cp examples/node/*.js $INSTALL_PREFIX
 
 # copy test files
 cp test/*.js $INSTALL_PREFIX
 
 # run tests
 cd $INSTALL_PREFIX
-node test_node.js ../example_files/oscli.xml
+# node test_node.js ../example_files/oscli.xml
 
 ls -l .
